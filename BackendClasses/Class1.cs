3cc64e2bcde6b3f4fb3e7b3d6b5ff379e5ee0776
Ben Kaiser <benjamin.kaiser@mines.sdsmt.edu>
2017-03-28 09:22:02 -0600

    initializing

---
+++ BackendClasses/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendClasses
+{
+    public class Card
+    {
+        private string _Suite;
+        private int _CardNumber;
+        private bool _Flipped;
+
+        public Card()
+        {
+            _Suite = "Clubs";
+            _CardNumber = 1;
+            _Flipped = false;
+        }
+
+        public Card(string suite, int card, bool flipped)
+        {
+            _Suite = suite;
+            _CardNumber = card;
+            _Flipped = flipped;
+        }
+
+        public int CardNumber
+        {
+            get
+            {
+                return _CardNumber;
+            }
+            set
+            {
+                _CardNumber = value;
+            }
+        }
+
+    }
+}
